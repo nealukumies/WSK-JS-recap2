4ae0ca49d47450ba58afd49f412da8040be3a537
nealukumies <nea.lukumies@metropolia.fi>
2025-03-19 09:48:01 +0200

    task4

---
+++ Task4/task4.js
@@ -0,0 +1,15 @@
+function sortArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+const numbers = [5, 10224, 8, 2, 12, 1, 98, 3, 23];
+console.log(sortArray(numbers));
